6c44699ccfa02dffcf4fb20c3ee62064af385b6c
mewild <michael@wildapplications.com>
2017-11-13 17:55:17 +0000

    Email service implementation

--- src/helpers/account.helper.js
+++ src/helpers/account.helper.js
@@ -207,7 +207,13 @@ account.create = function(call, callback){
                       return callback({message:JSON.stringify({code:'01000006', error:errors['0006']})}, null);
                     })
                   }else{
-                    callback(null, {token: generateToken(passwordResult._id)});
+                    emailClient.send({recipient:"michael@tabap.co.uk", subject:"Testing Email from Service", content:"Fuck me it worked"}, function(err, callback){
+                      if(err){
+                        return callback({message:JSON.stringify(err)}, null);
+                      }else{
+                        callback(null, {token: generateToken(passwordResult._id)});
+                      }
+                    })
                   }
                 })
 
