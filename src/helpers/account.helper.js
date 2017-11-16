f48939011238c8ed817c68c923060e6b08edb35e
mewild <michael@wildapplications.com>
2017-11-16 14:56:23 +0000

    Password recovery

--- src/helpers/account.helper.js
+++ src/helpers/account.helper.js
@@ -160,7 +160,7 @@ account.resetPassword = function(call, callback){
   if(call.request.guid && call.request.password){
     authenticationClient.resetPassword({guid: call.request.guid, password: call.request.password}, function(err, response){
       if(err){
-        return callback({message: err}, null);
+        return callback(err, null);
       }
       console.log("result", response);
       return callback(null, {successful:response.reset});
