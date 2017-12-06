145d39ddf11cec5a5123a46c8b50b63176b6babc
mewild <michael@wildapplications.com>
2017-12-06 11:45:48 +0000

    GCloud SQL

--- src/helpers/account.helper.js
+++ src/helpers/account.helper.js
@@ -10,7 +10,7 @@ var pool = mysql.createPool({
   connectionLimit : 10,
   host            : '127.0.0.1',
   port            : '3306',
-  user            : 'proxyuser',
+  user            : process.env.DB_USER,
   password        : process.env.DB_PASS,
   database        : 'usrdb'
 });
